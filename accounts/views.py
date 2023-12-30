60b0bf189eff4f30dd6f580834a97f9d6c1c7966
kainat999 <kainatbajwah999@gmail.com>
2023-12-30 07:56:44 +0000

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import RedirectView
+
+
+class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = 'http://127.0.0.1:8000/dj-rest-auth/login/'
+    client_class = OAuth2Client
+
+class GoogleLogin(SocialLoginView): # if you want to use Implicit Grant, use this
+    adapter_class = GoogleOAuth2Adapter
+
+
+
+
+
+class UserRedirectView(LoginRequiredMixin, RedirectView):
+    """
+    This view is needed by the dj-rest-auth-library in order to work the google login. It's a bug.
+    """
+
+    permanent = False
+
+    def get_redirect_url(self):
+        return "redirect-url"    
